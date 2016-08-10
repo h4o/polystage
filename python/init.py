88f420f40ff36d0b51f1a61ab3b320adb62a82af
Nicolas Lecourtois <lecourtoisn@gmail.com>
2016-08-10 16:38:53 +0200

    Add some methods to help the user

--- python/init.py
+++ python/init.py
@@ -1,13 +1,28 @@
+import os
 import requests
+from os.path import dirname, basename, isfile
+import glob
 from python.atlas import *
 from usr_scripts import *
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-print('Hello there :)')
-print('Type helpme() to know the commands available')
+print('\n\nHello there :)')
+print("\nType atlas() or excel() to see what's available\n")
 
 
-def helpme():
-    print('NOTHING YET !')
+def data():
+    print(', '.join(os.listdir('data')))
+
+
+def atlas():
+    mods = [file[:-3] for file in os.listdir('usr_scripts/atlas') if
+            file.endswith('.py') and not file.startswith('_')]
+    print(', '.join(mods))
+
+
+def excel():
+    mods = [file[:-3] for file in os.listdir('usr_scripts/excel') if
+            file.endswith('.py') and not file.startswith('_')]
+    print(', '.join(mods))
